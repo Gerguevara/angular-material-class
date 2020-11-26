5cb0107b700a7228e6a6675c2730cda45a5cf680
Gerardo Guevara <gerardoguevara64@gmail.com>
2020-11-25 22:30:11 -0600

    Material Icons done

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {MatButtonToggleModule} from '@angular/material/button-toggle';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatIconModule } from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
 const MaterialComponents = [
-  MatButtonToggleModule
+  MatButtonToggleModule,
+  MatButtonModule,
+  MatIconModule
+
 ];
 
 @NgModule({
