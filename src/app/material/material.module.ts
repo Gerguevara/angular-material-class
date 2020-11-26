65ef7d0db0e4a5a08d3c59f4f134a4362ee154e8
Gerardo Guevara <gerardoguevara64@gmail.com>
2020-11-25 21:46:17 -0600

    buttons done

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,10 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatSliderModule } from '@angular/material/slider';
+import {MatButtonModule} from '@angular/material/button';
 
 const MaterialComponents = [
-  MatSliderModule
-
+  MatButtonModule
 ];
 
 @NgModule({
