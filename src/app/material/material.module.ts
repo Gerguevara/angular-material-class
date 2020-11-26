dd06f34d2284d4495395b23e559c22017540e36c
Gerardo Guevara <gerardoguevara64@gmail.com>
2020-11-26 12:39:11 -0600

    sidebar header done

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
 
 const MaterialComponents = [
-  MatProgressSpinnerModule
+  MatToolbarModule,
+  MatIconModule,
+  MatButtonModule,
 ];
 
 @NgModule({
