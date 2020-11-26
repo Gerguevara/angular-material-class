3f07754f9ace1af38a15716683d748906e86cd9c
Gerardo Guevara <gerardoguevara64@gmail.com>
2020-11-25 22:21:21 -0600

    toogle buttons done

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,9 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {MatButtonModule} from '@angular/material/button';
-
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
 const MaterialComponents = [
-  MatButtonModule
+  MatButtonToggleModule
 ];
 
 @NgModule({
