417ec8e7c678a927f13a92cef864ee0715c1a3fa
Gerardo Guevara <gerardoguevara64@gmail.com>
2020-11-26 23:08:56 -0600

    list done

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -4,10 +4,11 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import { MatListModule } from '@angular/material/list';
 import { MatIconModule } from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
+import { MatButtonModule } from '@angular/material/button';
+import {MatMenuModule} from '@angular/material/menu';
 
 const MaterialComponents = [
-  MatSidenavModule, MatToolbarModule, MatListModule, MatIconModule, MatButtonModule
+  MatSidenavModule, MatToolbarModule, MatListModule, MatIconModule, MatButtonModule, MatMenuModule
 ];
 
 @NgModule({
