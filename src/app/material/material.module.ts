69430994addb61ace878e88e14417e5771924fc0
Gerardo Guevara <gerardoguevara64@gmail.com>
2020-11-27 13:26:33 -0600

    cards done

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -7,12 +7,11 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatDividerModule } from '@angular/material/divider';
-import { MatGridListModule } from '@angular/material/grid-list';
-import {MatExpansionModule} from '@angular/material/expansion';
+import {MatCardModule} from '@angular/material/card';
 
 const MaterialComponents = [
   MatSidenavModule, MatToolbarModule, MatListModule, MatIconModule, MatButtonModule, MatMenuModule
-  ,MatDividerModule , MatGridListModule, MatExpansionModule
+  ,MatDividerModule ,MatCardModule
 ];
 
 @NgModule({
