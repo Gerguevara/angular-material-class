78c896ab550ba0ef41668c0bfba6f4cf4b05f109
Gerardo Guevara <gerardoguevara64@gmail.com>
2020-11-28 13:07:01 -0600

    radi buttons and chackbox

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -12,12 +12,13 @@ import {MatFormFieldModule} from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatCardModule } from '@angular/material/card';
 import {MatSelectModule} from '@angular/material/select';
-import {MatAutocompleteModule} from '@angular/material/autocomplete'
-
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatRadioModule } from '@angular/material/radio';
+import {MatDatepickerModule} from '@angular/material/datepicker';
 const MaterialComponents = [
   MatSidenavModule, MatToolbarModule, MatListModule, MatIconModule, MatButtonModule, MatMenuModule
   , MatDividerModule, MatStepperModule, MatFormFieldModule, MatInputModule, MatCardModule, MatSelectModule,
-  MatAutocompleteModule
+  MatAutocompleteModule, MatRadioModule, MatDatepickerModule
 ];
 
 @NgModule({
